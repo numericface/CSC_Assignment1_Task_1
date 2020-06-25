68f6eeb7b2f7c4d155dc005812a790d54aca422e
numericface <56810849+numericface@users.noreply.github.com>
2020-06-26 00:35:59 +0800

    Add files via upload

---
+++ Danish_1828737_Weather_API/script/1828737_Task_1.js
@@ -0,0 +1,35 @@
+﻿var _PremiumApiBaseURL = 'http://api.worldweatheronline.com/premium/v1/';
+
+var _PremiumApiKey = 'f29ae2557c4543bf92c150139200706';
+
+// -------------------------------------------
+
+function JSONP_LocalWeather(input) {
+    var url = _PremiumApiBaseURL + 'weather.ashx?q=' + input.query + '&format=' + input.format + '&extra=' + input.extra + '&num_of_days=' + input.num_of_days + '&date=' + input.date + '&fx=' + input.fx + '&tp=' + input.tp + '&cc=' + input.cc + '&includelocation=' + input.includelocation + '&show_comments=' + input.show_comments + '&key=' + _PremiumApiKey;
+
+    jsonP(url, input.callback);
+}
+
+
+
+// -------------------------------------------
+
+// Helper Method
+function jsonP(url, callback) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        contentType: "application/json",
+        jsonpCallback: callback,
+        dataType: 'jsonp',
+        success: function (json) {
+            console.dir('success');
+        },
+        error: function (e) {
+            console.log(e.message);
+        }
+    });
+}
+
+
